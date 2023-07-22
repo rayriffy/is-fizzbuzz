150c190824d5d99920870375c39b3b5c6482a177
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-22 16:43:15 +0900

    feat: isFizz

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export const foo = 'bar'
+// this function will print fizz if the number is divisible by 3
+export const isFizz = (num: number): string => {
+  if (num % 3 === 0) return 'Fizz'
+  return ''
+}

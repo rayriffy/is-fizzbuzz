3d0c79fd3cb13a774c865c2d0d3075eee0da9245
Phumrapee Limpianchop <git@rayriffy.com>
2023-07-22 16:51:09 +0900

    feat: isBuzz

---
+++ .changeset/hungry-jars-pay.md
@@ -0,0 +1,5 @@
+---
+'@rayriffy/is-fizzbuzz': minor
+---
+
+add isBuzz function
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ export const isFizz = (num: number): string => {
   if (num % 3 === 0) return 'Fizz'
   return ''
 }
+
+// this function will print buzz if the number is divisible by 5
+export const isBuzz = (num: number): string => {
+  if (num % 5 === 0) return 'Buzz'
+  return ''
+}
